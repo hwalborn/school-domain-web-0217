9a7bc7eb76c922eb0f89bc5898abc35e89f3d6fc
hwalborn <hwalborn@gmail.com>
2017-02-08 11:21:24 -0500

    Class Dismissed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :roster
+
+  def initialize(school)
+    @roster = Hash.new()
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each{|k,v| v.sort!}
+  end
+end
